e168d5436bc1e424a31a5330a78333618a255ab6
lujianming-star <U202015784@hust.edu.cn>
2021-12-21 17:32:20 +0800

    second commit

--- training1_f103/MDK-ARM/hmi.c
+++ training1_f103/MDK-ARM/hmi.c
@@ -1,1 +1,18 @@
 #include "hmi.h"
+unsigned char buf[64];
+void HMISends(char *buf1);
+{
+	uint8_t i=0;
+	while(1)
+	{
+		if(buf1[i]!=0)
+		{
+			HAL_UART_Transmit(&huart2,buf1[i],sizeof(buf1[i]),10000);
+			while(HAL_UART_GetState(&huart2)!=RESET) {};  //ÅÐ¶Ï´®¿Ú×´Ì¬
+			i++;
+		}
+		else
+			return;
+	}
+}
+//void HMISendb(uint8_t k);
